f42355c4d78894e7b637266a12de396efdb23597
AnemKushi <153365206+AnemKushi@users.noreply.github.com>
2026-03-26 21:24:20 +0530

    Enhance start.sh with comments for better understanding
    Added comments for clarity on script usage.

--- backend/start.sh
+++ backend/start.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+# Go to backend folder
+cd backend
+
+# Activate virtualenv (optional, can skip if Railway installs requirements automatically)
+# source ../venv/Scripts/activate
+
+# Run FastAPI app
 uvicorn app.main:app --host 0.0.0.0 --port $PORT
