d4e7860af13f146ac371970e04f584077de65fd0
AnemKushi <ak2804ushi@gmail.com>
2026-03-26 21:49:42 +0530

    Remove broken start scripts

--- startt.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Install Python dependencies
-pip install --upgrade pip
-pip install -r backend/requirements.txt
-
-# Run FastAPI app
-uvicorn backend.run_server:app --host 0.0.0.0 --port $PORT
